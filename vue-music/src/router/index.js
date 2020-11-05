6f11b163c448b452bfe524903450d4e725631be6
chenzelong <420306751@qq.com>
2020-11-05 14:44:35 +0800

    删除一些文件

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-    }
-  ]
+const routes = [
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
